6207f35930bae58ca89216f8fc5a5a94e0b28669
Daniel Mircea <daniel@viseztrance.com>
2013-12-03 23:15:15 +0200

    Show ruby version in prompt (only for appropriate projects)

--- prompt.bash
+++ prompt.bash
@@ -1,30 +1,53 @@
 declare red="\e[31m"
+declare green="\e[92m"
 declare yellow="\e[93m"
 declare blue="\e[94m"
 declare cyan="\e[96m"
 declare wipe="\033[1m\033[0m"
 
-is_git_repo() {
+__is_git_repo() {
     git rev-parse --is-inside-work-tree > /dev/null 2>&1
 }
 
-is_dirty_repo() {
+__is_dirty_repo() {
     [ $(git diff --shortstat | wc -l) != "0" ]
 }
 
-get_git_branch() {
+__get_git_branch() {
     git symbolic-ref --short HEAD
 }
 
-get_version_control() {
-    local status="✓"
+__get_git_root() {
+    git rev-parse --show-toplevel
+}
 
-    if is_git_repo; then
-        if is_dirty_repo; then
-            status="✗"
+__is_ruby_project() {
+    [ -f "$(__get_git_root)/Gemfile" ]
+}
+
+__get_ruby_version() {
+    ruby --version | grep -Po '^\w+\s+\K.*?(?=\s+)'
+}
+
+__get_version_control_info() {
+    local status="✓"
+    if __is_dirty_repo; then
+        status="✗"
+    fi
+    echo -en "${blue}git:(${wipe}${red}$(__get_git_branch)${wipe}${blue})${wipe} ${yellow}$status${wipe} "
+}
+
+__get_ruby_info() {
+    echo -en "${red}⚡${wipe} ${blue}ruby:(${wipe}${red}$(__get_ruby_version)${wipe}${blue})${wipe} ${red}⚡${wipe} "
+}
+
+__get_development_prompt() {
+    if __is_git_repo; then
+        if __is_ruby_project; then
+            __get_ruby_info
         fi
-        echo -e "${blue}git:(${wipe}${red}$(get_git_branch)${wipe}${blue})${wipe} ${yellow}$status${wipe} "
+        __get_version_control_info
     fi
 }
 
-PS1="${red}➜${wipe} ${cyan}\W${wipe} \$(get_version_control)"
+PS1="${red}➜${wipe} ${cyan}\W${wipe} \$(__get_development_prompt)"
