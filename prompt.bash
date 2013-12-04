5369391e0542dfad37149965ad12a4b34467e328
Daniel Mircea <daniel@viseztrance.com>
2013-12-04 22:21:07 +0200

    Accommodate smaller viewports

--- prompt.bash
+++ prompt.bash
@@ -1,9 +1,9 @@
-declare red="\e[31m"
-declare green="\e[92m"
-declare yellow="\e[93m"
-declare blue="\e[94m"
-declare cyan="\e[96m"
-declare wipe="\033[1m\033[0m"
+declare red="\[\e[31m\]"
+declare green="\[\e[92m\]"
+declare yellow="\[\e[93m\]"
+declare blue="\[\e[94m\]"
+declare cyan="\[\e[96m\]"
+declare wipe="\[\033[1m\033[0m\]"
 
 __is_git_repo() {
     git rev-parse --is-inside-work-tree > /dev/null 2>&1
@@ -29,16 +29,16 @@ __get_ruby_version() {
     ruby --version | grep -Po '^\w+\s+\K.*?(?=\s+)'
 }
 
-__get_version_control_info() {
+__get_git_info() {
     local status="✓"
     if __is_dirty_repo; then
         status="✗"
     fi
-    echo -en "${blue}git:(${wipe}${red}$(__get_git_branch)${wipe}${blue})${wipe} ${yellow}$status${wipe} "
+    echo -n "${blue}git:(${wipe}${red}$(__get_git_branch)${wipe}${blue})${wipe} ${yellow}$status${wipe} "
 }
 
 __get_ruby_info() {
-    echo -en "${red}⚡${wipe} ${blue}ruby:(${wipe}${red}$(__get_ruby_version)${wipe}${blue})${wipe} ${red}⚡${wipe} "
+    echo -n "${red}⚡${wipe} ${blue}ruby:(${wipe}${red}$(__get_ruby_version)${wipe}${blue})${wipe} ${red}⚡${wipe} "
 }
 
 __get_development_prompt() {
@@ -46,8 +46,12 @@ __get_development_prompt() {
         if __is_ruby_project; then
             __get_ruby_info
         fi
-        __get_version_control_info
+        __get_git_info
     fi
 }
 
-PS1="${red}➜${wipe} ${cyan}\W${wipe} \$(__get_development_prompt)"
+set_prompt() {
+    PS1="${red}➜${wipe} ${cyan}\W${wipe} $(__get_development_prompt)"
+}
+
+PROMPT_COMMAND=set_prompt
