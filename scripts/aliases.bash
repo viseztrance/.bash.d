9314fa0f74fe5710ea8987efbb2734f5e36f683e
Daniel Mircea <daniel@viseztrance.com>
2021-04-06 22:47:48 +0300

    Add command that shows per process ram usage

--- scripts/aliases.bash
+++ scripts/aliases.bash
@@ -2,6 +2,7 @@ alias reload!="source ~/.bashrc && echo \"Bash config reloaded!\""
 
 alias be="bundle exec"
 alias r="bundle exec rspec"
+alias ram="ps aux | awk '{print \$6/1024 \" MB\t\t\" \$11}'  | sort -n"
 
 if which exa &> /dev/null; then
   alias ls="exa"
